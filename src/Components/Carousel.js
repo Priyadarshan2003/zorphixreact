5395988af626bdf1e136b9aa8f19c1881ee2985a
Priyadarshan N S <nspriyadharshan@gmail.com>
2023-09-04 19:51:17 +0530

    added images to the carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,33 +1,94 @@
 import '../App.css';
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import img1 from '../img/2fact.png'
+import img1 from '../img/eventphotos/year 1/DSC_0026.JPG'
+import img2 from '../img/eventphotos/year 1/DSC_0034.JPG'
+import img3 from '../img/eventphotos/year 1/DSC_0191.JPG'
+import img4 from '../img/eventphotos/year 1/DSC_0217.JPG'
+import img5 from '../img/eventphotos/year 1/DSC_0382.JPG'
+import img6 from '../img/eventphotos/year 1/DSC_0436.JPG'
+import img7 from '../img/eventphotos/year 2/IMG_2736.JPG'
+import img8 from '../img/eventphotos/year 2/IMG_4396.JPG'
+import img9 from '../img/eventphotos/year 2/IMG_4841.JPG'
+import img10 from '../img/eventphotos/DSC_0510.JPG'
 
 const CarouselComponent = () => {
   return (
-    <Carousel className='carousel' interval={1500}>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={img1}
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={img1}
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={img1}
-          alt="Third slide"
-        />
-      </Carousel.Item>
-    </Carousel>
+    <div className='container-fluid'>
+    <Carousel className='carousel' interval={2000}>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={img1}
+        alt="First slide"
+      />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={img2}
+        alt="Second slide"
+      />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={img3}
+        alt="Third slide"
+      />
+    </Carousel.Item>
+    {/* Add more Carousel.Items for additional images */}
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={img4}
+        alt="Fourth slide"
+      />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={img5}
+        alt="Fifth slide"
+      />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={img6}
+        alt="Sixth slide"
+      />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={img7}
+        alt="Seventh slide"
+      />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={img8}
+        alt="Eighth slide"
+      />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={img9}
+        alt="Ninth slide"
+      />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={img10}
+        alt="Tenth slide"
+      />
+    </Carousel.Item>
+  </Carousel>
+  </div>
   );
 };
 
--- src/Components/Sponsors.js
+++ src/Components/Sponsors.js
@@ -1,59 +1,42 @@
-import React from 'react'
-import git from '../img/git.png';
-import wolfr from '../img/wolfr.png';
-import do_logo from '../img/DO_Logo_Vertical_White.png';
-import people_logo from '../img/people_logo-removebg-preview.png';
-import cake_logo from '../img/cake_logo_white.png';
-
+import React from "react";
+import git from "../img/git.png";
+import wolfr from "../img/wolfr.png";
+import do_logo from "../img/DO_Logo_Vertical_White.png";
+import people_logo from "../img/people_logo-removebg-preview.png";
+import cake_logo from "../img/cake_logo_white.png";
+import "../App.css";
 
 const Sponsors = () => {
-    return (
-        <div class="section-features">
-
-            <div class="u-center-text u-margin-bottom-big">
-                <h2 class="heading-secondary white letter-spacing">Sponsored By</h2>
-            </div>
-            <div class="sponsor">
-                <div class="github-sponsor">
-                    <div class="section-sponsor-card">
-                        <img src={git} class="git" alt="github" />
-                    </div>
-                </div>
-                <div class="row1">
-                    <div class="section-sponsor-card">
-                        <img
-                            src={wolfr}
-                            class="i padding wol"
-                            alt="wolfram"
-                        />
-                    </div>
-
-                    <div class="section-sponsor-card">
-                        <img
-                            src={do_logo}
-                            class="i ocean"
-                            alt="digitalOcean"
-                        />
-                    </div>
-                    <div class="section-sponsor-card">
-                        <img
-                            src={people_logo}
-                            alt="people's logo"
-                            class="i some-large"
-                        />
-                    </div>
-
-                    <div class="section-sponsor-card">
-                        <img
-                            src={cake_logo}
-                            class="i padding-not"
-                            alt="interviewcake"
-                        />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div class="section-features">
+      <div class="u-center-text u-margin-bottom-big">
+        <h2 class="heading-secondary white letter-spacing">Sponsored By</h2>
+      </div>
+      <div class="sponsor">
+        <div class="github-sponsor">
+          <div class="section-sponsor-card">
+            <img src={git} class="git" alt="github" />
+          </div>
         </div>
-    )
-}
+        <div class="row1">
+          <div class="section-sponsor-card">
+            <img src={wolfr} class="i padding wol" alt="wolfram" />
+          </div>
+
+          <div class="section-sponsor-card">
+            <img src={do_logo} class="i ocean" alt="digitalOcean" />
+          </div>
+          <div class="section-sponsor-card">
+            <img src={people_logo} alt="people's logo" class="i some-large" />
+          </div>
+
+          <div class="section-sponsor-card">
+            <img src={cake_logo} class="i padding-not" alt="interviewcake" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Sponsors;
