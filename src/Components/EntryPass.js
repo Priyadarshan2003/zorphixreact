3a181695035d89ca6b4addeb7cddd1351975a05f
Priyadarshan N S <nspriyadharshan@gmail.com>
2023-09-05 23:16:00 +0530

    edited contents of the qr code

--- src/Components/EntryPass.js
+++ src/Components/EntryPass.js
@@ -6,6 +6,22 @@ const EntryPass = () => {
   
   const userData = JSON.parse(localStorage.getItem("userData"));
 
+  const qrCodeValue = `
+    ID: ${userData.zorid}
+    Name: ${userData.fullName}
+    College: ${userData.college}
+    Degree: ${userData.degree}
+    Dept: ${userData.dept}
+    Year: ${userData.year}
+    Event1 : ${userData.event1}
+    Event2 : ${userData.event2}
+    Event3 : ${userData.event3}
+    Event4 : ${userData.event4}
+    Event5 : ${userData.event5}
+    Workshop1 : ${userData.workshop1}
+    Workshop2 : ${userData.workshop2}
+  `;
+
   return (
     <div id="EntryPass" className="section-EntryPass">
       <div class="u-center-text">
@@ -19,7 +35,7 @@ const EntryPass = () => {
               </div>
             ) : (
                <div className="qrdiv">
-                 <QRCode className="qrcodeimage" value = {userData.zorid} size={200} />
+                 <QRCode className="qrcodeimage" value = {qrCodeValue} size={200} />
                 </div> 
             )}
     </div>
